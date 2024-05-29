746ccdcc57bab2fa6acb9ff08a45da0b52090979
Hirw Hope <gakundohope5@gmail.com>
2024-05-29 20:23:33 +0200

    updates

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 
 const app = express();
-const PORT = 5003;
+const PORT =process.env.PORT || 5333;
 
 // Mock prediction data for demonstration
 const predictionData = {
@@ -19,18 +19,26 @@ app.use(cors());
 app.use(bodyParser.json());
 
 // Endpoint to get earthquake magnitude prediction for a district
-app.get('/prediction/earthquake/:district', (req, res) => {
-  const { district } = req.params;
-  const earthquakeMagnitude = getRandomValue(predictionData.earth_quake_magnitude);
-  res.json({ district, earthquakeMagnitude });
+app.get('/prediction/earthquake/:district',async (req, res) => {
+  try{
+    const { district } = req.params;
+    const earthquakeMagnitude = getRandomValue(predictionData.earth_quake_magnitude);
+    return res.status(200).json({ district, earthquakeMagnitude });
+  }catch(error){
+    res.status(500).json({ error: error.message})
+  }
 });
 
 // Endpoint to get landslide size and flooding size prediction for a district
-app.get('/prediction/landslide_flooding/:district', (req, res) => {
+app.get('/prediction/landslide_flooding/:district', async(req, res) => {
+  try{
   const { district } = req.params;
   const landslideSize = getRandomValue(predictionData.landslide_size);
   const floodingSize = getRandomValue(predictionData.flooding_size);
-  res.json({ district, landslideSize, floodingSize });
+  return res.status(200).json({ district, landslideSize, floodingSize }); 
+  }catch(error){
+  res.status(500).json({ error: error.message})
+}
 });
 
 // Generate random value from array (mock function)
